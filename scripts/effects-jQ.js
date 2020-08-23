387acb3f161718ec817b3eb5c860afd5aef656e2
PAPPY-JOE <fatoyejoseph@gmail.com>
2020-08-23 23:57:52 +0100

    Added onload animation

--- scripts/effects-jQ.js
+++ scripts/effects-jQ.js
@@ -6,13 +6,31 @@ let observer1 = new IntersectionObserver(function(entries) {
         // To check if it works
         // console.log('Element is fully visible in screen');
         $('.services-text p').fadeIn(1250);
-        $('.services-text p a').fadeIn(1250);
+        $('.services-text p a').fadeIn(1250, function () {
+            
+            $('.box-1 p, .box-1 button').slideDown(800, function () {
+                
+                $('.box-2 p, .box-2 button').slideDown(800, function () {
+                  
+                    $('.box-3 p, .box-3 button').slideDown(800);
+                });
+            });
+        });
     }
     else if (entries[0].isIntersecting === false){
         // To check if it works     
         // console.log('Element is not fully visible in screen');
         $('.services-text p').fadeOut(150);
-        $('.services-text p a').fadeOut(150);
+        $('.services-text p a').fadeOut(150, function () {
+            
+            $('.box-1 p, .box-1 button').slideUp(300, function () {
+                
+                $('.box-2 p, .box-2 button').slideUp(300, function () {
+                  
+                    $('.box-3 p, .box-3 button').slideUp(300);
+                });
+            });
+        });
     }        
     
 }, { threshold: [0] });
